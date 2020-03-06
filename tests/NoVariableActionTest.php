affc9bb5454164f9dbf51285bae78d0502eba193
Till Brede <tb@symcon.de>
2020-03-06 13:21:55 +0100

    - replaced EdgeCaseTest with a descriptice Name - test reduced to the necessary

---
+++ tests/NoVariableActionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/TestBase.php';
+
+class NoVariableActionTest extends TestBase
+{
+    public function testNoVariableAction()
+    {
+        //Variables
+        $variableID = $this->CreateActionVariable(VAR_INT, 'Variable1', false);
+
+        //Instances
+        $instanceID = $this->AnwesenheitsSimulationID;
+
+        //Configuration
+        IPS_SetConfiguration($instanceID, json_encode(
+            [
+                'Targets' => json_encode([
+                    [
+                        'VariableID' => $variableID,
+                        'Name'       => 'Var1'
+                    ]
+                ]),
+                'RequiredSwitchCount' => 1
+            ]
+        ));
+        IPS_ApplyChanges($instanceID);
+        $caption = json_decode(IPS_GetConfigurationForm($instanceID), true)['elements'][0]['caption'];
+        $this->assertEquals($caption, "The following variables have no action and therefore cannot be switched:\n - Variable1");
+    }
+}
