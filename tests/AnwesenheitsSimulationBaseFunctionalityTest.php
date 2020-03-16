73c39a72077171f158f8f113107e92af996038de
Till Brede <tb@symcon.de>
2020-03-16 11:28:08 +0100

    renamed base test

--- tests/AnwesenheitsSimulationBaseFunctionalityTest.php
+++ tests/AnwesenheitsSimulationBaseFunctionalityTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 include_once __DIR__ . '/TestBase.php';
 
-class AnwesenheitsSimulationTest extends TestBase
+class AnwesenheitsSimulationBaseFunctionalityTest extends TestBase
 {
     public function testBaseFunctionality()
     {
